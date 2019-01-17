5bcd7d8c483965d378e44564d955614fc2890192
matt1007k <matt1007k@gmail.com>
2019-01-16 20:43:55 -0500

    Modificando estilos y logo de la plantilla de admin

--- prhotel/settings.py
+++ prhotel/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'prhotel/static') 
 ]
+
+# Media Folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
